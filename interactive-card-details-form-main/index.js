29c3626c44b216bc0b032c57a417f46e8202d291
Tini <156963175+Tini369@users.noreply.github.com>
2024-04-19 21:34:55 +0200

    index.js

--- interactive-card-details-form-main/index.js
+++ interactive-card-details-form-main/index.js
@@ -1,29 +1,44 @@
-function myFunction(){
-    var name = document.getElementById("name").value;
-    var cardNumber = document.getElementById("cardNumber").value;
-    var userMonth = document.getElementById("month").value;
-    var userYear = document.getElementById("year").value;
-    var userCvc = document.getElementById("cvc").value;
-    //this displays the inputs onto the card
+function myFunction() {
+  var name = document.getElementById("name").value;
+  var cardNumber = document.getElementById("cardNumber").value;
+  var userMonth = document.getElementById("month").value;
+  var userYear = document.getElementById("year").value;
+  var userCvc = document.getElementById("cvc").value;
+
+  //   let errorMsg = "";
+  if (name.length === 0) {
+    // errorMsg += "Please enter your name. ";
+    alert("Please enter your name");
+    return;
+  } else {
     document.getElementById("userName").innerHTML = name;
+  }
+
+  if (cardNumber.length === 0) {
+    alert("Please enter your credit card number");
+    return;
+  } else {
     document.getElementById("cardNums").innerHTML = cardNumber;
+  }
+
+  if (userMonth.length === 0) {
+    alert("Please enter your credit card expiration date");
+    return;
+  } else {
     document.getElementById("userMonth").innerHTML = userMonth;
+  }
+
+  if (userYear.length === 0) {
+    alert("Please enter your credit card expiration date");
+    return;
+  } else {
     document.getElementById("userYear").innerHTML = userYear;
+  }
+
+  if (userCvc.length === 0) {
+    alert("Please enter your credit card cvc number");
+    return;
+  } else {
     document.getElementById("userCvc").innerHTML = userCvc;
-    //this puts an alert if the user does not enter his credit card information
-    if (name.length === 0) {
-        alert("Please enter your name"); 
-    }
-    if (cardNumber.length === 0) {
-        alert("Please enter your credit card number");
-    }
-    if (userMonth.length === 0) {
-        alert("Please enter your credit card expiration date"); 
-    }
-    if (userYear.length === 0) {
-        alert("Please enter your credit card expiration date"); 
-    }
-    if (userCvc.length === 0) {
-        alert("Please enter your credit card cvc number"); 
-    }
+  }
 }
